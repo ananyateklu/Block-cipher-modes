2d7a38fe79f87b7125b19c9ac37c539d121021d6
Cassie Schultz <CassieSchultz35@gmail.com>
2020-09-30 18:44:24 -0500

    Working on a method for encoding still. I have gotten as far as converting the plain text to the binary string

--- Lab3.py
+++ Lab3.py
@@ -1,9 +1,29 @@
+import binascii
 def main():
     plaintext = input("Enter plaintext")
-    for i in range(0,len(plaintext)):
-        decimal =[]
-        decimal.append(ord(plaintext[i])) 
-        print(decimal)
+    encode(plaintext, "poop")
+
+
+
+
+def encode(plaintext, key):
+    decimal_vals = []
+    # Type cast plaintext to acii chars
+    for i in range(0, len(plaintext)):
+        decimal_vals.append(ord(plaintext[i]))
+    # Type cast decimal to binary
+    binary_text = []
+    conversion = '' 
+    for i in range(0,len(decimal_vals)):
+        conversion = bin(decimal_vals[i])
+        conversion = conversion[2:]
+        for j in range(0,len(conversion)):
+            binary_text.insert((7*i+j), int(conversion[j]))
+    print(binary_text)
+    # for i in range(0, len(plaintext)):
+    #     block[i] = plaintext[i+3%35]
+    #     print(plaintext)
 
 
 main()
+#def decrypt(ciphertext, key):
