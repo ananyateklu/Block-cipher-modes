a91307d62d908ff60e0e5f15d5e2bfb4fca34cae
Cassie Schultz <CassieSchultz35@gmail.com>
2020-09-30 17:20:57 -0500

    I got a start on the main/ how input will work

--- Lab3.py
+++ Lab3.py
@@ -1,6 +1,9 @@
 def main():
     plaintext = input("Enter plaintext")
-    for ch in plaintext:
-        decimal = []
-        decimal[ch] = ord(plaintext[ch])
-        print(plaintext)
+    for i in range(0,len(plaintext)):
+        decimal =[]
+        decimal.append(ord(plaintext[i])) 
+        print(decimal)
+
+
+main()
