90e6b9d5d4ef76f0344d0afab89cdc10b65221b5
Ananya Teklu <teklu009@morris.umn.edu>
2020-10-01 10:30:54 -0500

    Implement decode. it doesnt work yet.

--- Lab3.py
+++ Lab3.py
@@ -1,8 +1,8 @@
 import binascii
 def main():
-    plaintext = input("Enter plaintext")
-    encode(plaintext, "a5Z#\t")
-
+    plaintext = input("Enter plaintext ")
+    print(encode(plaintext, "a5Z#\t"))
+    decode([0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],"a5Z#\t")
 def convert_bin(acii):
     decimal_vals = []
     # Type cast plaintext to acii chars
@@ -21,12 +21,13 @@ def convert_bin(acii):
             conversion = padstr+conversion                
         for j in range(0,len(conversion)):
             binary_text.insert((7*i+j), int(conversion[j]))
-    return binary_text
+    return binary_text  
 
 
 def encode(plaintext, key):
     # convert plaintext to binary
     binary_plain = convert_bin(plaintext)
+   
     # convert key to binary
     binary_key = convert_bin(key)
     # shift the plaintext three to the right
@@ -36,9 +37,34 @@ def encode(plaintext, key):
     # add key mod 2
     xor_bits = []
     for i in range(0, len(binary_key)):
-        xor_bits.append((binary_shift[i]+binary_key[i])%2)
+        xor_bits.append((int(binary_shift[i])+ binary_key[i])%2)
     return xor_bits
-    
+
+def decode(cipherblock,key):
+    # convert key to binary
+    binary_key = convert_bin(key)
+    # shift the cipher block three to the left(reverse diffusion)
+    binary_reverse_shift = []
+    for i in range(0,len(cipherblock)):
+        binary_reverse_shift.insert((i-3)%35,cipherblock[i])
+    # add key mod 2
+    xor_bits = []
+    for i in range(0, len(cipherblock)):
+        xor_bits.append(((binary_reverse_shift[i])+ binary_key[i])%2)
+    binary_combined = ""
+    for i in xor_bits:
+        binary_combined += str(i)
+    print(binary_combined)
+    split_binary = " ".join(binary_combined[i:i+7] for i in range(0, len(binary_combined), 7))  
+    print(split_binary)
+    ascii_string = ""
+    for i in split_binary.split():
+        an_integer = int(i,2)
+        ascii_character = chr(an_integer)
+        ascii_string += ascii_character
+    print(ascii_string)
+  
+
 
 main()
 #def decrypt(ciphertext, key):
