0380cd9e137a073caa9a69e1516d7cda4010bad0
Cassie Schultz <CassieSchultz35@gmail.com>
2020-10-02 12:46:31 -0500

    some clean up and clarity

--- Lab3.py
+++ Lab3.py
@@ -2,10 +2,16 @@
 import math as m
 
 def main():
-    plaintext = input("Enter plaintext")
-    blocks = []
-    binary_plain = convert_bin(plaintext)
-    blocks = to_blocks(binary_plain)
+    # get key and plaintext from user
+    plaintext = input("Enter plaintext ")
+    key = "a5Z#\t"
+    print(key)
+    blocks = [] 
+    # convert plain to blocks of binary
+    blocks = to_blocks(plaintext)
+    # encode blocks
+    for i in range(0,len(blocks)):
+        print(encode(blocks[i],key))
     #encode(plaintext, "a5Z#\t")
     
 
@@ -31,23 +37,23 @@ def convert_bin(acii):
 
 
 def encode(plaintext, key):
-    # convert plaintext to binary
-    binary_plain = convert_bin(plaintext)
     # convert key to binary
     binary_key = convert_bin(key)
     # shift the plaintext three to the right
     binary_shift = [] 
-    for i in range(0, len(binary_plain)):
-        binary_shift.insert((i+3)%35,binary_plain[i]) 
+    for i in range(0, len(plaintext)):
+        binary_shift.insert((i+3)%35,plaintext[i]) 
     # add key mod 2
     xor_bits = []
     for i in range(0, len(binary_key)):
         xor_bits.append((binary_shift[i]+binary_key[i])%2)
     return xor_bits
 
-def to_blocks(binary_text):
+def to_blocks(plaintext):
     block_size = 35
     block_list = []
+    #convert plaintext to binary
+    binary_text = convert_bin(plaintext)
     # Check to see if text is already 35 bits
     if (len(binary_text) == block_size):
         block_list.insert(0, binary_text)
